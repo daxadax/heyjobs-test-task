327d8ec16a7c15ff65155405136ffa17474d414c
dax <d.dax@email.com>
2019-05-15 13:21:53 +0200

    Add assumptions as a note

--- lib/services/generate_diff.rb
+++ lib/services/generate_diff.rb
@@ -1,3 +1,9 @@
+# NOTE: This class makes the following assumptions:
+# 1. 'local' is an object
+# 2. 'remote' is a hash
+# 3. 'remote' has a key 'reference'
+# 4. 'local' and 'remote' have at least one matching attribute/key combo
+
 module Services
   class GenerateDiff
     def self.call(local, remote)
